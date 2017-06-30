d21351a1dc1cd5499b9bcc4c5555ae00e3aa10e8
sriram333 <sriramsai30@gmail.com>
2017-06-30 18:53:08 +0530

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,51 @@
+﻿namespace FacialRecognitionDoor
+{
+    /// <summary>
+    /// General constant variables
+    /// </summary>
+    public static class GeneralConstants
+    {
+        // This variable should be set to false for devices, unlike the Raspberry Pi, that have GPU support
+        public const bool DisableLiveCameraFeed = false;
+
+        // Oxford Face API Primary should be entered here
+        // You can obtain a subscription key for Face API by following the instructions here: https://www.microsoft.com/cognitive-services/en-us/sign-up
+        public const string OxfordAPIKey = "60adec7975394e2a800e6430099ae9b5";
+        
+        // Name of the folder in which all Whitelist data is stored
+        public const string WhiteListFolderName = "Facial Recognition Door Whitelist";
+
+        public static string IntruderFolderName = "Facial Recognition Door Intruderlist";
+    }
+
+    /// <summary>
+    /// Constant variables that hold messages to be read via the SpeechHelper class
+    /// </summary>
+    public static class SpeechContants
+    {
+        public const string InitialGreetingMessage = "Welcome to iSeguro! speech has been initialized.";
+        public const string IntruderDetectedMessage = "Intruder detected.";
+        public const string VisitorNotRecognizedMessage = "Sorry! I don't recognize you, so I cannot open the door.";
+        public const string NoCameraMessage = "Sorry! It seems like your camera has not been fully initialized.";
+
+        public static string GeneralGreetigMessage(string visitorName)
+        {
+            return "Welcome " + visitorName + "! You are authorised to be here,I will open the door";
+        }
+    }
+
+    /// <summary>
+    /// Constant variables that hold values used to interact with device Gpio
+    /// </summary>
+    public static class GpioConstants
+    {
+        // The GPIO pin that the doorbell button is attached to
+        public const int ButtonPinID = 5;
+
+        // The GPIO pin that the door lock is attached to
+        public const int DoorLockPinID = 4;
+
+        // The amount of time in seconds that the door will remain unlocked for
+        public const int DoorLockOpenDurationSeconds = 10;
+    }
+}
